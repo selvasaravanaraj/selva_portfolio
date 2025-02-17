a73575f4cb3cff0f8b362d9865a69ac1b3596276
selvasaravanaraj <selvasaravanarajj@gmail.com>
2025-02-17 11:46:11 +0530

    fix

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/selva-resume-org - Copy.pdf";
+import pdf from "../../Assets/../Assets/selva_RESUME.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
